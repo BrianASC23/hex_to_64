74807b745673c218a68214579a133ba96e794f6a
BrianASC23 <bcfrags@gmail.com>
2024-09-26 21:08:18 -0400

    Second Commit

--- HexTo64.py
+++ HexTo64.py
@@ -1,50 +1,30 @@
+def hex_to_64(hex_input):
+    base64_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-"
+    bytes = ""
 
+    length_of_hex = len(hex_input)
+    for i in range(0, length_of_hex, 2):
+        hex_pair = hex_input[i:i+2]
+        decimal = int(hex_pair, 16)
+        binary = bin(decimal)[2:]
+        binary = '0' * (8 - len(binary)) + binary
+        bytes = bytes + binary
 
-def hex_to_base_64(input):
     
-    hex_list = input.split()
+    output = ""
+    length_of_string = len(bytes)
+    for i in range(0, length_of_string, 6):
+        six_bits = bytes[i:i+6]
+        print("sixbits", six_bits)
+        while len(six_bits) < 6:
+           
+            six_bits = six_bits + '0'
     
-    base64_array = []
-    
-    for hex_num, hex in enumerate(hex_list):
-        sum = 0
-        word = ''
-        for i, char in enumerate(hex):
-            if i == 0:
-                exp = 1
-            elif i == 1:
-                exp = 0
-            if char.isdigit():
-                num = char
-            else:
-                num = ord(char) - ord('a') + 10
-        
-            sum += int(num) * 16**(exp)
-        #Convert to Base 64
-        while sum != 0:
-            remainder = sum % 64
-            print("remainder is", remainder)
-            if remainder < 26:
-                remainder = chr(ord('A') + remainder)
-            elif remainder < 51:
-                remainder = chr(ord('A') + remainder + 6)
-            elif remainder < 62:
-                remainder = chr(remainder)
-            elif remainder == 62:
-                remainder = '+'
-            elif remainder == 63:
-                remainder = '-'
-
-            word = remainder + word
-            sum = (sum)//64 
-            
-        base64_array.append(word)   
-    return base64_array
+        key = int(six_bits, 2)
+        output += base64_key[key]
 
 
-
-
-
-
+    print(output)
+    return output
 
 
